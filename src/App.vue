ed0d719d59bfd8b32e542f71c557baaa8079352f
Judielle Villa <judiellezinke@yahoo.de>
2022-07-09 18:31:23 +0200

    added landingpage component and implemented it

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <Navbar/>
+  <Landing/>
 </template>
 
 <script>
 
 import Navbar from './components/Navbar.vue'
+import Landing from './components/Landing.vue'
 
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    Landing
   }
 }
 </script>
@@ -22,5 +25,5 @@ export default {
     box-sizing: border-box;
   }
 
-  
+
 </style>
---
+++ src/components/Landing.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'LandingPage'
+    }
+</script>
+
+<style scoped>
+    section {
+        background-color: rosybrown;
+        width: 100vw;
+        height: calc(100vh - 2rem);
+    }
+</style>
